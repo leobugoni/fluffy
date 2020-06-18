30b41df44bcc73b70df69ba438c8948e57dce4a1
Leonardo Bugoni <leobugoni@gmail.com>
2020-06-18 14:46:07 -0300

    Init Spring Project

---
+++ src/main/java/io/github/leobugoni/fluffy/FluffyApplication.java
@@ -0,0 +1,13 @@
+package io.github.leobugoni.fluffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluffyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluffyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leobugoni/fluffy/FluffyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leobugoni.fluffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluffyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
